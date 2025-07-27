adc826414107a31ed08ce950e001b9f527247db3
Eduardo Garro <efgarro@yahoo.com>
2025-07-27 12:52:54 -0600

    netlify-b

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import * as fs from "node:fs";
 import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
 
-const filePath = "./src/count.txt";
+const filePath = "./public/count.txt";
 
 async function readCount() {
   return parseInt(
