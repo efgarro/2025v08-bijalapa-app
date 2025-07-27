9336ff51dd644dad7d1808e9fe5c65feafe83958
Eduardo Garro <efgarro@yahoo.com>
2025-07-27 12:00:57 -0600

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+
+import { cloudflare } from "unenv";
+import nitroCloudflareBindings from "nitro-cloudflare-dev";
+
+const untypedOptions = {
+  unenv: cloudflare,
+  modules: [nitroCloudflareBindings],
+} as any; // Current type definitions are incomplete, casting needed
+
+export default defineConfig({
+  server: {
+    port: 3030,
+  },
+  plugins: [
+    tsConfigPaths(),
+    tanstackStart({ customViteReactPlugin: true, target: 'cloudflare-module', ...untypedOptions }),
+    viteReact(),
+  ],
+})
